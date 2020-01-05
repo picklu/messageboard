531a0563fa8344d38dae3d1722fd1e0cde398811
Subrata Sarker <subrata_sarker@yahoo.com>
2020-01-05 22:34:07 +0900

    Use separate file for controllers

---
+++ controllers/handlers.js
@@ -0,0 +1,187 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+const MONGODB_CONNECTION_STRING = process.env.DB_LOCAL || process.env.DB;
+const DB_NAME = process.env.DB_NAME;
+
+const controllers = {};
+
+// connect to the database
+controllers.connectDB = async function () {
+    let result;
+    try {
+        let client = await MongoClient.connect(MONGODB_CONNECTION_STRING, { useUnifiedTopology: true });
+        result = { client: client, db: client.db(DB_NAME) };
+    }
+    catch (error) {
+        result = { error: error };
+    }
+    finally {
+        return result;
+    }
+}
+
+// Create a new thread to a board
+controllers.insertThread = async function (collectionName, data) {
+    const dbConn = await controllers.connectDB();
+    if (dbConn.error) {
+        console.log("== db insert data error ==>", dbConn.error);
+        return { error: dbConn.error };
+    }
+
+    const { client, db } = dbConn;
+    const collection = db.collection(collectionName);
+    let result;
+    try {
+        result = await collection.insertOne(data);
+    }
+    catch (error) {
+        result = { error: error };
+    }
+    finally {
+        if (client) {
+            await client.close();
+        }
+        return result;
+    }
+}
+
+// Create a new reply to a thread in a board
+controllers.insertReply = async function (collectionName, data) {
+    const dbConn = await controllers.connectDB();
+    if (dbConn.error) {
+        console.log("== db insert data error ==>", dbConn.error);
+        return { error: dbConn.error };
+    }
+
+    const { client, db } = dbConn;
+    const collection = db.collection(collectionName);
+    let result;
+    try {
+        result = await collection.insertOne(data);
+    }
+    catch (error) {
+        result = { error: error };
+    }
+    finally {
+        if (client) {
+            await client.close();
+        }
+        return result;
+    }
+}
+
+// get all threads in a board
+controllers.getThreads = async function (collectionName) {
+    const dbConn = await controllers.connectDB();
+    if (dbConn.error) {
+        return { error: dbConn.error };
+    }
+    const { client, db } = dbConn;
+    const collection = db.collection(collectionName);
+    let result;
+    try {
+        result = await collection.aggregate([
+            {
+                $project: {
+                    _id,
+                    text,
+                    reported,
+                    created_on,
+                    bumped_on,
+                    replies,
+                    replycount: { $size: '$replies' }
+                }
+            }
+        ]).toArray();
+    }
+    catch (error) {
+        result = { error: error };
+    }
+    finally {
+        if (client) {
+            await client.close();
+        }
+        return result;
+    }
+}
+
+// get all replies in a thread
+controllers.getReplies = async function (collectionName, id) {
+    const dbConn = await controllers.connectDB();
+    if (dbConn.error) {
+        return { error: dbConn.error };
+    }
+    const { client, db } = dbConn;
+    const collection = db.collection(collectionName);
+    let result;
+    try {
+        result = await collection.findOne({ _id: id });
+    }
+    catch (error) {
+        result = { error: error };
+    }
+    finally {
+        if (client) {
+            await client.close();
+        }
+        return result;
+    }
+}
+
+// delete a thread in a board
+controllers.deleteThread = async function (collectionName, id) {
+    const dbConn = await controllers.connectDB();
+    if (dbConn.error) {
+        return { error: dbConn.error };
+    }
+    const { client, db } = dbConn;
+    const collection = db.collection(collectionName);
+    let result;
+    try {
+        if (id) {
+            result = await collection.findOneAndDelete({ _id: id });
+        }
+        else {
+            result = await collection.deleteMany({});
+        }
+    }
+    catch (error) {
+        result = { error: error };
+    }
+    finally {
+        if (client) {
+            await client.close();
+        }
+        return result;
+    }
+}
+
+// delete a reply of a thread
+controllers.deleteReply = async function (collectionName, id) {
+    const dbConn = await controllers.connectDB();
+    if (dbConn.error) {
+        return { error: dbConn.error };
+    }
+    const { client, db } = dbConn;
+    const collection = db.collection(collectionName);
+    let result;
+    try {
+        if (id) {
+            result = await collection.findOneAndDelete({ _id: id });
+        }
+        else {
+            result = await collection.deleteMany({});
+        }
+    }
+    catch (error) {
+        result = { error: error };
+    }
+    finally {
+        if (client) {
+            await client.close();
+        }
+        return result;
+    }
+}
+
+module.exports = controllers;
--- routes/api.js
+++ routes/api.js
@@ -8,200 +8,34 @@
 
 'use strict';
 
+
 var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB_LOCAL || process.env.DB;
-const DB_NAME = process.env.DB_NAME;
-
-var client;
-
-async function closeDBConnection() {
-  if (client) {
-    await client.close();
-    client = undefined;
-  }
-  return;
-}
-
-// connect to the database
-async function connectDB() {
-  let result;
-  if (!client) {
-    try {
-      client = await MongoClient.connect(MONGODB_CONNECTION_STRING, { useUnifiedTopology: true });
-      result = { db: client.db(DB_NAME) };
-    }
-    catch (error) {
-      result = { error: error };
-    }
-    finally {
-      return result;
-    }
-  }
-  return { error: 'Previous connection was not closed!' };
-}
-
-// Create a new thread to a board
-async function insertThread(collectionName, data) {
-
-  const dbConn = await connectDB();
-  if (dbConn.error) {
-    console.log("== db insert data error ==>", dbConn.error);
-    return { error: dbConn.error };
-  }
-  const db = dbConn.db;
-  const collection = db.collection(collectionName);
-  let result;
-  try {
-    result = await collection.insertOne(data);
-  }
-  catch (error) {
-    result = { error: error };
-  }
-  finally {
-
-    await closeDBConnection();
-    return result;
-  }
-}
-
-// Create a new reply to a thread in a board
-async function insertReply(collectionName, data) {
-
-  const dbConn = await connectDB();
-  if (dbConn.error) {
-    console.log("== db insert data error ==>", dbConn.error);
-    return { error: dbConn.error };
-  }
-  const db = dbConn.db;
-  const collection = db.collection(collectionName);
-  let result;
-  try {
-    result = await collection.insertOne(data);
-  }
-  catch (error) {
-    result = { error: error };
-  }
-  finally {
-
-    await closeDBConnection();
-    return result;
-  }
-}
-
-// get all threads in a board
-async function getThreads(collectionName) {
-  const dbConn = await connectDB();
-  if (dbConn.error) {
-    return { error: dbConn.error };
-  }
-  const db = dbConn.db;
-  const collection = db.collection(collectionName);
-  let result;
-  try {
-    result = await collection.aggregate([
-      {
-        $project: {
-          _id: 1,
-          title: 1,
-          commentcount: { $size: '$comments' }
-        }
-      }
-    ]).toArray();
-  }
-  catch (error) {
-    result = { error: error };
-  }
-  finally {
-    await closeDBConnection();
-    return result;
-  }
-}
-
-// get all replies in a thread
-async function getReplies(collectionName, id) {
-  const dbConn = await connectDB();
-  if (dbConn.error) {
-    return { error: dbConn.error };
-  }
-  const db = dbConn.db;
-  const collection = db.collection(collectionName);
-  let result;
-  try {
-    result = await collection.findOne({ _id: id });
-  }
-  catch (error) {
-    result = { error: error };
-  }
-  finally {
-    await closeDBConnection();
-    return result;
-  }
-}
-
-// delete a thread in a board
-async function deleteThread(collectionName, id) {
-  const dbConn = await connectDB();
-  if (dbConn.error) {
-    return { error: dbConn.error };
-  }
-  const db = dbConn.db;
-  const collection = db.collection(collectionName);
-  let result;
-  try {
-    if (id) {
-      result = await collection.findOneAndDelete({ _id: id });
-    }
-    else {
-      result = await collection.deleteMany({});
-    }
-  }
-  catch (error) {
-    result = { error: error };
-  }
-  finally {
-    await closeDBConnection();
-    return result;
-  }
-}
-
-// delete a reply of a thread
-async function deleteReply(collectionName, id) {
-  const dbConn = await connectDB();
-  if (dbConn.error) {
-    return { error: dbConn.error };
-  }
-  const db = dbConn.db;
-  const collection = db.collection(collectionName);
-  let result;
-  try {
-    if (id) {
-      result = await collection.findOneAndDelete({ _id: id });
-    }
-    else {
-      result = await collection.deleteMany({});
-    }
-  }
-  catch (error) {
-    result = { error: error };
-  }
-  finally {
-    await closeDBConnection();
-    return result;
-  }
-}
-
+var controllers = require('../controllers/handlers');
 
 module.exports = function (app) {
 
   app.route('/api/threads/:board')
-    .get(function (req, res) {
-      res.json([])
+    .get(async function (req, res) {
+      const board = req.params.board;
+      const result = await controllers.getThreads(board);
+
+      if (result && result.error) {
+        res.json([]);
+      }
+      else if (result && Array.isArray(result)) {
+        res.json(result);
+      }
     })
 
     .post(function (req, res) {
+      const now = new Date().toISOString();
+      const board = req.params.board;
+      const data = req.body;
+      data.created_on = now;
+      data.bumped_on = now;
+      data.reported = false;
 
+      res.redirect('/b/' + board + '/');
     })
 
     .put(function (req, res) {

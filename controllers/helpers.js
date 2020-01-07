b22b015b5c84a05edf2a83e959c3f7caaddf0b07
Subrata Sarker <subrata_sarker@yahoo.com>
2020-01-08 01:30:57 +0900

    Add a helper function log

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -4,10 +4,16 @@ helpers.trace = function (message = '') {
     const stack = new Error().stack;
     const trace = stack.split('\n')[2];
     const match = /\((.*)\)/.exec(trace);
-    console.log(`== @ ==> ${match[1]}`);
+    console.log(`<=== ${match[1]}`);
     console.log(message);
-    console.log(`== @ ==> ${match[1]}`);
+    console.log(`== ${match[1]}===>`);
 
+}
+
+helpers.log = function (message = '') {
+    console.log('<==============================');
+    console.log(message);
+    console.log('==============================>');
 }
 
 module.exports = helpers;

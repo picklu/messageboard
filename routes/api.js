145c5b2059607ff707497ce6ec9d0f6b27bbb056
Subrata Sarker <subrata_sarker@yahoo.com>
2020-01-08 21:21:40 +0900

    Fix if/else statement for incorrect password

--- routes/api.js
+++ routes/api.js
@@ -94,8 +94,11 @@ module.exports = function (app) {
             return res.send('success');
           }
           else {
-            return res.send('incorrect password');
+            return res.send('fail');
           }
+        }
+        else {
+          return res.send('incorrect password');
         }
       }
       else {
